390b8e79f489cdc7378dc88316d18a6a17c6319c
Sonalibhandare6612 <swapnalib1897@gmail.com>
2023-08-08 15:41:54 +0530

    changes

--- store/models.py
+++ store/models.py
@@ -10,13 +10,14 @@ class Category(models.Model):
     
     
     def __str__(self):
-        return self.name
+        return self.name 
         
     
     
 class Products(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=50)
+    product_id = models.IntegerField(default=0)
     price = models.IntegerField(default=0)
     description = models.CharField(max_length=200, default=" ", null=True, blank=True)
     image = models.ImageField(upload_to='uploads/products/')
