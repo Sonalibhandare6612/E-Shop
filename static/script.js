60f7c826dec848961c575f2ec9b797102317c34f
Sonalibhandare6612 <swapnalib1897@gmail.com>
2023-08-08 15:28:57 +0530

    changes

--- static/script.js
+++ static/script.js
@@ -1,6 +1,21 @@
-// Add event listener to the button
-const likeButton = document.getElementById('likeButton');
-likeButton.addEventListener('click', function() {
-  // Toggle the 'clicked' class to apply the color change
-  likeButton.classList.toggle('clicked');
+
+// Add click event listener to all like buttons
+const likeButtons = document.querySelectorAll('.likeButton');
+
+likeButtons.forEach(likeButton => {
+  likeButton.addEventListener('click', function() {
+    // Get the card ID from the data attribute
+    const cardId = likeButton.getAttribute('data-card-id');
+    
+    // Toggle the 'clicked' class to apply the color change for the specific button
+    likeButton.classList.toggle('clicked');
+    console.log("reached");
+    
+    // You can also send an AJAX request here to update the like status on the server
+    // Example using fetch:
+    // fetch(`/update-like/${cardId}/`)
+    //   .then(response => response.json())
+    //   .then(data => console.log(data));
+  });
 });
+
