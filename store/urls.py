609d64f654cc5fd020982a9d2f6abafd2dab4cf0
Sonalibhandare6612 <swapnalib1897@gmail.com>
2023-08-11 17:34:53 +0530

    changed

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("", views.order, name="order"),
+    path("signup", views.signup, name="signup"),
 ]
