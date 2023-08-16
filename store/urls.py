92abb146578a377de1c10857b60c6e6c1918d3e8
Sonalibhandare6612 <swapnalib1897@gmail.com>
2023-08-16 15:00:34 +0530

    changes

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.Index.as_view, name="index"),
+    path("", views.Index.as_view(), name="index"),
     path("order", views.order, name="order"),
     path("signup", views.Signup.as_view(), name="Signup"),
     path("login", views.Login.as_view(), name="Login"),
