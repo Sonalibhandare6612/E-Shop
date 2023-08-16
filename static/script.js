47ce16801f0c441a647eed7c26ee66611227455a
Sonalibhandare6612 <swapnalib1897@gmail.com>
2023-08-16 16:42:17 +0530

    changes

--- static/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const button = document.getElementById('color-change-button');
-
-  button.addEventListener('click', function() {
-      button.classList.add('clicked');
-  });
-});
